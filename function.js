8c8ce187f7a79f8982c3367d76e6d012e6b54bf3
Angelo Gonçalves <contatoproangelo@gmail.com>
2026-07-28 10:30:07 -0300

    add 4.2

--- function.js
+++ function.js
@@ -38,3 +38,77 @@ const nomePessoa = 'Angelo';
 const sobrenomePessoa ='Clangs';
 
 console.log (mensagem3 (nomePessoa, sobrenomePessoa));
+
+// e isso permite utilizar variáveis de outros tipos
+
+function mensagem4 (nome, sobrenome, idade) {
+    const saudacao = 'Olá, '+ nome + ' '+ sobrenome + '!';
+    const exibeIdade = 'Você tem ' + idade + ' anos.';
+    return saudacao + ' ' + exibeIdade;
+}
+
+
+console.log (mensagem3 (nomePessoa, sobrenomePessoa, 18));
+console.log (mensagem3 ('Mathias', 'Kalecinsky', 19));
+
+/////////////////////////////////////////////////
+
+function calculaDespesa(luz, internet) {
+    const despesaTotal = luz + internet;
+    return 'Esse mês, a despesa foi de: R$ ' + despesaTotal;
+}
+
+console.log(calculaDespesa (230,125));
+
+////////////////////////////////////////////////
+
+function mostrarFrase (frases){
+    return frases;
+}
+
+console.log (mostrarFrase('Vamo que vamo!'));
+
+//////////////////////////////////////////////
+
+function saudacao(nome) {
+    return 'Olá, ' + nome + '!';
+}
+
+console.log (saudacao('Júlia'));
+
+///////////////////////////////////////
+
+function clientePedido (numeroPedido){
+    if (numeroPedido === 1) {
+        return 'Pizza de Calabresa';
+    }
+    else if (numeroPedido === 2){
+        return 'Pizza de Quatro Queijos';
+    }
+    else if (numeroPedido === 3){
+        return 'Pizza de Frango com Catupiry';
+    }
+    else if (numeroPedido === 4){
+        return 'Pizza de Brigadeiro';
+    }
+    else {
+        return 'Número do pedido INVÁLIDO!';
+    }
+}
+console.log (clientePedido(1));
+
+////////////////////////////////////////
+
+function mediaNumeros (arrayNumeros){
+    let soma = 0;
+    let media = 0;
+    
+    for (let i = 0; i < arrayNumeros.length; i++){
+        soma = soma + arrayNumeros [i];
+    }
+    media = soma / arrayNumeros.length;
+    return media;
+}
+
+console.log (mediaNumeros(arrayNumeros = [2,3,4,5]));
+
